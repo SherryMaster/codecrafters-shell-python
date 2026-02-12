0d796e1eb476e2e0899ab70968d6d709e558d83e
Shaheer Ahmed <128972759+SherryMaster@users.noreply.github.com>
2026-02-12 14:36:01 +0500

    fix: remove extra space in command not found message

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ import sys
 def main():
     sys.stdout.write("$ ") 
     command = input()
-    sys.stdout.write(f"{command}:  command not found\n")
+    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
