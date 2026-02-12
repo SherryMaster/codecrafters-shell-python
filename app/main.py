79cd6aaa90b42ffdd37f9341176c4381797b7c1a
Shaheer Ahmed <128972759+SherryMaster@users.noreply.github.com>
2026-02-12 18:23:53 +0500

    fix: add handling for 'type' command in main loop

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import sys
 
 
 def main():
+    builtins = ["echo", "exit", "type"]
     while True:
         sys.stdout.write("$ ") 
         command = input()
@@ -10,6 +11,13 @@ def main():
         elif command.split()[0] == "echo":
             _, *args = command.split()
             sys.stdout.write(" ".join(args) + "\n")
+        elif command.split()[0] == "type":
+            _, *args = command.split()
+            for arg in args:
+                if arg in builtins:
+                    sys.stdout.write(f"{arg} is a shell builtin\n")
+                else:
+                    sys.stdout.write(f"{arg}: not found\n")
         else:
             sys.stdout.write(f"{command}: command not found\n")
 
